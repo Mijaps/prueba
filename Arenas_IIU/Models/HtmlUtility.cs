f1917773577b2b1d097b679a6cd71277145ad8d9
chano64123 <promo201674@gmail.com>
2022-09-19 23:10:45 -0500

    configuracion de proyecto

---
+++ Arenas_IIU/Models/HtmlUtility.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Arenas_IIU.Models {
+    public static class HtmlUtility {
+        public static string IsActive(this HtmlHelper html,
+                                 string control,
+                                 string action) {
+            var routeData = html.ViewContext.RouteData;
+
+            var routeControl = (string)routeData.Values["controller"];
+
+            // must match both
+            var returnActive = control == routeControl;
+
+
+            return returnActive ? "nav-link active" : "nav-link";
+        }
+    }
+}
---
+++ Arenas_IIU/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general sobre un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie los valores de estos atributos para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Arenas_IIU")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Arenas_IIU")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si ComVisible se establece en false, los componentes COM no verán los 
+// tipos de este ensamblado. Si necesita obtener acceso a un tipo de este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como ID de typelib si este proyecto se expone a COM
+[assembly: Guid("8d9c0926-bdef-497e-81a9-0d1e4c15484a")]
+
+// La información de versión de un ensamblado consta de los siguientes cuatro valores:
+//
+//      Versión principal
+//      Versión secundaria 
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión 
+// mediante el carácter '*', como se muestra a continuación:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
